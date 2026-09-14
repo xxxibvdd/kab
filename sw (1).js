b3d84e210fc583a68ae3859aef78f74531db2537
xxxibvdd <budsaikhan.s@gmail.com>
2026-09-15 01:52:02 +0800

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,13 @@
+const CACHE = 'nertomyo-v1';
+const FILES = ['./', './index.html', './icon-180.png', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
